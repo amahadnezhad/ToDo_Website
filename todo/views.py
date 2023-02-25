301a13eb1ce24db245363a8bf9e38de8883f2b6d
amahadnezhad <amahadnezhad@gmail.com>
2023-02-25 14:50:45 +0330

    Add InsertForm To Template

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import Task
+from .forms import ListForm
 
 
 class AboutUsView(generic.TemplateView):
@@ -9,5 +10,12 @@ class AboutUsView(generic.TemplateView):
 
 
 def tasksview(request):  # Home
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            items = Task.objects.all()
+            return render(request, 'home.html', {'items': 'items'})
+
     items = Task.objects.all
     return render(request, 'home.html', {'items': items})
