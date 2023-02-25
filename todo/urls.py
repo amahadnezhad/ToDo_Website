8d0b8055d0e867b9066b8b33d2e0443cc047a253
amahadnezhad <amahadnezhad@gmail.com>
2023-02-25 13:55:33 +0330

    Create tasksview

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AboutUsView
+from .views import AboutUsView, tasksview
 
 urlpatterns = [
+    path('', tasksview, name='home'),
     path('aboutus/', AboutUsView.as_view(), name='aboutus'),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import Task
+
 
 class AboutUsView(generic.TemplateView):
     template_name = 'about.html'
+
+
+def tasksview(request):  # Home
+    items = Task.objects.all
+    return render(request, 'home.html', {'items': items})
