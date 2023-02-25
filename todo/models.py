55332ac589789b4666b883eaa3cc93600e1d013e
amahadnezhad <amahadnezhad@gmail.com>
2023-02-25 13:47:13 +0330

    Create Task Model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    item = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.item
